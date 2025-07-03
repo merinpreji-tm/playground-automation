9c486a7a5923f72f387f5ad3e93ee6aff7096f2c
Merin P Reji <merin.reji@testingmavens.com>
2025-07-03 15:15:48 +0530

    TC20

--- testAssets/tests/playground.test.ts
+++ testAssets/tests/playground.test.ts
@@ -643,4 +643,59 @@ test.describe("Test the Playground web application", async () => {
       expect(wishlistItemCount, "Number of items in the wishlist should be zero").toBe(true);
     });
   });
+
+  test("TC20 - Verify if the user is able to add the product to the cart which is in wishlist", async ({ common, homePage, shopPage, productDetailsPage }) => {
+    await test.step(`Navigate to '${playgroundData.navigationMenu.shop}' page using menu option`, async () => {
+      await homePage.clickNavigationMenu(playgroundData.navigationMenu.shop);
+      const isShopVisible = await shopPage.verifyMenuIsVisible();
+      expect(isShopVisible, "Shop text should be displayed").toBe(true);
+    });
+
+    await test.step(`Filter the product listing by selecting the category '${playgroundData.filters.category.brandToSelect.mobiles}'`, async () => {
+      await shopPage.applyFilter(playgroundData.filters.category.sectionTitle, playgroundData.filters.category.brandToSelect.mobiles);
+    });
+
+    await test.step(`Verify that the results are related to ${playgroundData.filters.category.sectionTitle} '${playgroundData.filters.category.brandToSelect.mobiles}'`, async () => {
+      const resultsAreValid = await shopPage.verifyFilterResults(playgroundData.filters.category.sectionTitle, playgroundData.filters.category.brandToSelect.mobiles);
+      expect(resultsAreValid, `Products are of ${playgroundData.filters.category.sectionTitle} '${playgroundData.filters.category.brandToSelect.mobiles}'`).toBe(true);
+    });
+
+    await test.step(`Click on the first product and verify that product page displays the selected product`, async () => {
+      const title = await shopPage.clickProduct();
+      mobileTitle = await common.getText(productDetailsPage.productTitle);
+      expect(title, "Selected product title should be same as title displayed in product details page").toBe(mobileTitle);
+    });
+
+    await test.step("Click on 'Add to Wishlist' button and verify the success message displayed", async () => {
+      wishlistItemsCount = await common.getCount(homePage.wishlistItemsCount);
+      await common.clickButton(playgroundData.buttons.addToWishlist);
+      const successMessage = await productDetailsPage.getSuccessMessage();
+      expect(successMessage, `Auto-disappearing banner content displayed at the top right side of the page should be '${playgroundData.messages.wishlistSuccess}'`).toBe(playgroundData.messages.wishlistSuccess);
+    });
+
+    await test.step("Verify that the wishlist shows the number of products added", async () => {
+      const wishlistItemCountIncreased = await homePage.hasCountIncreased(wishlistItemsCount, homePage.wishlistItemsCount);
+      expect(wishlistItemCountIncreased, "Number of items in the wishlist should be increased").toBe(true);
+    });
+
+    await test.step("Navigate to wishlist", async () => {
+      await homePage.goToWishlist();
+      const pageTitle = await common.getText(common.pageTitle);
+      expect(pageTitle, `Page title should be '${playgroundData.titles.wishlist}'`).toBe(playgroundData.titles.wishlist);
+    });
+
+    await test.step("Click on 'Add to Cart' button", async () => {
+      await common.clickButton(playgroundData.buttons.addToCart);
+      const cartItemCountIncreased = await homePage.hasCountIncreased(cartItemsCount, homePage.cartItemsCount);
+      expect(cartItemCountIncreased, "Number of items in the cart should be increased").toBe(true);
+    });
+
+    // Removing item from wishlist
+    await test.step("Click on 'Reset Wishlist' button", async () => {
+      await common.clickButton(playgroundData.buttons.resetWishlist);
+      const wishlistItemCount = await homePage.hasCountBecomeZero(homePage.wishlistItemsCount);
+      expect(wishlistItemCount, "Number of items in the wishlist should be zero").toBe(true);
+    });
+  });
+
 });
