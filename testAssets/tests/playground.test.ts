8811cb677dc9c439753763002ea4a9bb277891b5
Merin P Reji <merin.reji@testingmavens.com>
2025-07-04 14:57:27 +0530

    Update playground.test.ts

--- testAssets/tests/playground.test.ts
+++ testAssets/tests/playground.test.ts
@@ -646,7 +646,7 @@ test.describe("Test the Playground web application", async () => {
     });
   });
 
-  test.skip("TC20 - Verify if the user is able to add the product to the cart which is in wishlist", async ({ common, homePage, shopPage, productDetailsPage }) => {
+  test("TC20 - Verify if the user is able to add the product to the cart which is in wishlist", async ({ common, homePage, shopPage, productDetailsPage }) => {
     await test.step(`Navigate to '${playgroundData.navigationMenu.shop}' page using menu option`, async () => {
       await homePage.clickNavigationMenu(playgroundData.navigationMenu.shop);
       const isShopVisible = await shopPage.verifyMenuIsVisible();
