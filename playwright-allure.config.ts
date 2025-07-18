ad4c766c6445a61eda92ca6c3d1a96922f8ef64c
Merin P Reji <merin.reji@testingmavens.com>
2025-07-18 16:12:01 +0530

    generated separate allure report

--- playwright-allure.config.ts
+++ playwright-allure.config.ts
@@ -13,19 +13,24 @@ dotenv.config({ path: path.resolve(__dirname, 'env', '.env.test') });
  */
 export default defineConfig({
   testDir: './testAssets/tests',
+//   testMatch: [
+//     'wishlist.test.ts',
+//     'e2e.test.ts',
+//     'playground.test.ts',
+//   ],
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  // workers: process.env.CI ? 1 : undefined,
+  workers: 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'html',
   reporter: [
-    ['html', { open: 'never' }],
-    ["allure-playwright", { resultsDir: "testArtifacts/allure-results", detail: false, }],
+    ['./utils/customReporter.ts'] 
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   // use: {
